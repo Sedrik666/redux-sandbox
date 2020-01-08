cc22cc31572994f045d3e8eb904a09b169af1e0e
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-08 15:45:17 +0200

    feat(reduxReact): first use react with redux

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from "redux";
+
+import Counter from './counter';
+
 import reducer from "./reducer";
 import * as actions from './actions'
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
-//custom bindActionCreators
-/*const bindActionCreator = (creator, dispatch) => (...args) => {
-    dispatch(creator(...args))
-};*/
-
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-document
-    .getElementById('inc')
-    .addEventListener('click', inc);
-
-document
-    .getElementById('dec')
-    .addEventListener('click', dec);
-
-document
-    .getElementById('rnd')
-    .addEventListener('click', () => {
-        const random  = Math.floor(Math.random() * 10);
-        rnd(random);
-    });
-
 const update = () => {
-    document
-        .getElementById('counter')
-        .innerHTML = store.getState()
+    ReactDOM.render(
+        <Counter counter={store.getState()}
+                 inc={inc}
+                 dec={dec}
+                 rnd={() => {
+                     const value = Math.floor(Math.random() * 10);
+                     rnd(value);
+                 }}
+        />,
+        document.getElementById('root')
+    );
 };
 
+update();
 store.subscribe(update);
+
